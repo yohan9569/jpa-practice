4211a1a8d939679e02d1408808ce5bc71135f290
Aaron Ryu <fbcndah@gmail.com>
2024-10-27 16:09:59 +0900

    실습 5-50: Page Controller 와 Json Controller 분리

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,60 +2,43 @@ package com.example.demo.controller;
 
 import com.example.demo.controller.dto.UserCreateRequestDto;
 import com.example.demo.controller.dto.UserResponseDto;
-import com.example.demo.service.IRepository;
 import com.example.demo.service.UserService;
 import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/users")
+@RequestMapping("/api/users")
 @RequiredArgsConstructor
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class UserController {
     UserService userService;
 
-    @Autowired
-    private ApplicationContext applicationContext;
-
-    @GetMapping("/bean")
-    @ResponseBody
-    public String bean() {
-        return applicationContext.getBean(IRepository.class).toString();
-    }
-
     @GetMapping("")
-    public String userPage(Model model) {
-        List<UserResponseDto> users = userService.findAll();
-        model.addAttribute("users", users);
-        return "/users/list";
+    public ResponseEntity<List<UserResponseDto>> users() {
+        try {
+            List<UserResponseDto> users = userService.findAll();
+            return ResponseEntity
+//                  .status(HttpStatusCode.valueOf(200))
+                    .status(HttpStatus.OK)      // 1. HTTP Status Code
+                    .body(users);               // 2. 결과 객체(List<User>)
+        } catch (RuntimeException e) {
+            return ResponseEntity
+//                  .status(HttpStatusCode.valueOf(404))
+                    .status(HttpStatus.NOT_FOUND)
+                    .body(null);
+        }
     }
 
-    @GetMapping("/detail")
-    public String detailPage(@RequestParam Integer id, Model model) {
-        UserResponseDto user = userService.findById(id);
-        model.addAttribute("id", user.getId());
-        model.addAttribute("name", user.getName());
-        model.addAttribute("age", user.getAge());
-        model.addAttribute("job", user.getJob());
-        model.addAttribute("specialty", user.getSpecialty());
-        return "/users/detail";
-    }
-
-    @GetMapping("/data")
-    @ResponseBody
-    public ResponseEntity<UserResponseDto> detailData(@RequestParam Integer id) {
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDto> user(@PathVariable Integer id) {
         try {
             UserResponseDto user = userService.findById(id);
             return ResponseEntity
---
+++ src/main/java/com/example/demo/controller/UserPageController.java
@@ -0,0 +1,40 @@
+package com.example.demo.controller;
+
+import com.example.demo.controller.dto.UserResponseDto;
+import com.example.demo.service.UserService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/users")
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+public class UserPageController {
+    UserService userService;
+
+    @GetMapping("")
+    public String usersPage(Model model) {
+        List<UserResponseDto> users = userService.findAll();
+        model.addAttribute("users", users);
+        return "/users/list";
+    }
+
+    @GetMapping("/{id}")
+    public String userPage(@PathVariable Integer id, Model model) {
+        UserResponseDto user = userService.findById(id);
+        model.addAttribute("id", user.getId());
+        model.addAttribute("name", user.getName());
+        model.addAttribute("age", user.getAge());
+        model.addAttribute("job", user.getJob());
+        model.addAttribute("specialty", user.getSpecialty());
+        return "/users/detail";
+    }
+}
